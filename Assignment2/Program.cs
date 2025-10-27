2a0d17d34219397df026dbf76cdb421e3bc3d7a6
yanal <yanal@YANALALKILANI>
2025-10-27 13:46:25 +0300

    Add project files.

---
+++ Assignment2/Program.cs
@@ -0,0 +1,55 @@
+﻿Console.WriteLine("Enter the number of mounth :");
+int NumOfMounth = Convert.ToInt32(Console.ReadLine());
+if (NumOfMounth < 1 || NumOfMounth > 12)
+{
+    Console.WriteLine("Invalid month number");
+}
+else if (NumOfMounth == 1)
+{
+    Console.WriteLine("January has 31 days");
+}
+if (NumOfMounth == 2)
+{
+    Console.WriteLine("February has 28 days");
+}
+if (NumOfMounth == 3)
+{
+    Console.WriteLine("March has 31 days");
+}
+if (NumOfMounth == 4)
+{
+    Console.WriteLine("April has 30 days");
+}
+if (NumOfMounth == 5)
+{
+    Console.WriteLine("May has 31 days");
+}
+if (NumOfMounth == 6)
+{
+    Console.WriteLine("June has 30 days");
+}
+if (NumOfMounth == 7)
+{
+    Console.WriteLine("July has 31 days");
+}
+if (NumOfMounth == 8)
+{
+    Console.WriteLine("August has 31 days");
+}
+if (NumOfMounth == 9)
+{
+    Console.WriteLine("September has 30 days");
+}
+if (NumOfMounth == 10)
+{
+    Console.WriteLine("October has 31 days");
+}
+if (NumOfMounth == 11)
+{
+    Console.WriteLine("November has 30 days");
+}
+if (NumOfMounth == 12)
+{
+    Console.WriteLine("December has 31 days");
+}
+
